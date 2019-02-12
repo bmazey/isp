5f363e0b8fa8b9b5e0d69805e1e906e97cef152d
Brandon <b.mazey@nyu.edu>
2019-02-12 17:24:04 -0500

    ok changing it up - let's make a rainbow table!

---
+++ assignment1/rainbow.py
@@ -0,0 +1,11 @@
+# TODO - implement password cracking technique!
+# opening development branch
+
+
+def create_table():
+    # TODO - make a rainbow table!
+    return
+
+
+if __name__ == '__main__':
+    create_table()
